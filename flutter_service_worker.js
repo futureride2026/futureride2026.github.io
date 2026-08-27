58f84743a503d9093361ef1888f729e5bdfb13ae
Gowtham Muraharirao <murahariraogowtham2000@gmail.com>
2026-08-28 02:51:42 +0530

    connection fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -9,20 +9,23 @@ self.addEventListener('activate', (event) => {
     (async () => {
       try {
         await self.registration.unregister();
-      } catch (error) {
-        console.warn('Failed to unregister the service worker:', error);
+      } catch (e) {
+        console.warn('Failed to unregister the service worker:', e);
       }
 
       try {
-        const clients = await self.clients.matchAll({type: 'window'});
+        const clients = await self.clients.matchAll({
+          type: 'window',
+        });
+        // Reload clients to ensure they are not using the old service worker.
         clients.forEach((client) => {
           if (client.url && 'navigate' in client) {
             client.navigate(client.url);
           }
         });
-      } catch (error) {
-        console.warn('Failed to refresh a service worker client:', error);
+      } catch (e) {
+        console.warn('Failed to navigate some service worker clients:', e);
       }
-    })(),
+    })()
   );
 });
